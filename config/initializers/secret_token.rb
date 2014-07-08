c54f0d071f2a7485ae3e03011a157ad0fe99ea54
Fernando Shayani <shayani@gmail.com>
2014-07-08 12:18:22 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geraldoapp::Application.config.secret_token = 'c78bbcb54567b1333b283b4255f2bc2823412339ee873c4392d7c06aaf736735df8fbc3c4f17af74484e2229b0e660c6cbe6c724f3bd14e925797eca86d2a685'
